f143a332aba245879dc8224e8c7189909a4d3f1d
Prashant <prashant@shiptoscale.com>
2025-04-30 14:17:33 +0530

    stashroom initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+
+export const metadata: Metadata = {
+  title: "StashRoom : secure, organized digital storage",
+  description: "StashRoom is your secure, organized digital storeroom for files, documents, and media.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { navItems } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  fullName: string;
+  avatar: string;
+  email: string;
+}
+
+const Sidebar = ({ fullName, avatar, email }: Props) => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <div className="lg:flex flex-col items-start justify-start hidden h-auto">
+          <Image
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={120}
+            height={24}
+            className="h-auto"
+          />
+          <h1 className="text-5xl text-brand font-semibold -mt-8">StashRoom</h1>
+        </div>
+
+        <div className="lg:hidden flex-col items-start justify-start flex h-auto">
+          <Image
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={120}
+            height={24}
+            className="h-auto"
+          />
+          <h1 className="text-5xl text-brand font-semibold -mt-8">StashRoom</h1>
+        </div>
+      </Link>
+
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active",
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active",
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+
+      <Image
+        src="/assets/images/files-2.png"
+        alt="logo"
+        width={506}
+        height={418}
+        className="w-full"
+      />
+
+      <div className="sidebar-user-info">
+        <Image
+          src={avatar}
+          alt="Avatar"
+          width={44}
+          height={44}
+          className="sidebar-user-avatar"
+        />
+        <div className="hidden lg:block">
+          <p className="subtitle-2 capitalize">{fullName}</p>
+          <p className="caption">{email}</p>
+        </div>
+      </div>
+    </aside>
+  );
+};
+export default Sidebar;
